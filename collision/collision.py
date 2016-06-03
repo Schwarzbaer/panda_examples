298bca15383461d00f0671ad92d8c4666052c6a6
baribal <TheCheapestPixels@gmail.com>
2016-06-03 22:01:22 +0200

    Cleaned up collision example dir.

--- collision/collision.py
+++ collision/collision.py
@@ -1,56 +1,59 @@
 #!/usr/bin/env python
 
-import sys
-import random
+from math import sin
 
-from direct.showbase.ShowBase import ShowBase
-from panda3d.core import PStatClient
-from direct.task import Task
+from panda3d.core import Vec3
+from panda3d.core import CollisionNode, CollisionSphere, CollisionRay
+from panda3d.core import CollisionHandlerQueue, CollisionTraverser
 
-class Base(ShowBase):
+from boilerplate.base import Base
+
+
+class SphereThatGetsCollidedWith:
     def __init__(self):
-        # The basics
-        ShowBase.__init__(self)
-        base.disableMouse()
-        base.setBackgroundColor(0, 0, 0)
-        base.setFrameRateMeter(True)
-        PStatClient.connect()
-        self.accept("escape", sys.exit)
-        # Camera
-        self.camera_orbit = base.render.attach_new_node("Camera orbit")
-        self.camera_pitch = self.camera_orbit.attach_new_node("Camera pitch")
-        base.camera.reparent_to(self.camera_pitch)
-        base.camera.set_pos(0, -10, 0)
-        # Camera control
-        self.camera_movement = (0, 0)
-        self.accept("arrow_up",       self.change_camera_movement, [ 0, -1])
-        self.accept("arrow_up-up",    self.change_camera_movement, [ 0,  1])
-        self.accept("arrow_down",     self.change_camera_movement, [ 0,  1])
-        self.accept("arrow_down-up",  self.change_camera_movement, [ 0, -1])
-        self.accept("arrow_left",     self.change_camera_movement, [-1,  0])
-        self.accept("arrow_left-up",  self.change_camera_movement, [ 1,  0])
-        self.accept("arrow_right",    self.change_camera_movement, [ 1,  0])
-        self.accept("arrow_right-up", self.change_camera_movement, [-1,  0])
-        base.taskMgr.add(self.move_camera, "Move camera")
-        # Object
-        self.setup_scene()
-        self.model = self.create_model()
-        self.model.reparent_to(base.render)
+        # Target; a sphere. You should attach it to your actual model,
+        # not .show() it directly. Also, the sphere you see is a low-
+        # poly model, but the collisions will be calculated against a
+        # mathematically perfect sphere.
+        self.target = CollisionSphere(0, 0, 0, 1)
+        self.target_nodepath = base.render.attach_new_node(CollisionNode('collision_target'))
+        self.target_nodepath.node().addSolid(self.target)
+        self.target_nodepath.show()
+        base.taskMgr.add(self.move_collidee, "Moving Target Task")
 
-    def create_model(self):
-        return loader.loadModel("models/smiley")
+    def move_collidee(self, task):
+        self.target_nodepath.set_pos(sin(task.time) * 3, 0, 0)
+        return task.cont
 
-    def change_camera_movement(self, turn, pitch):
-        self.camera_movement = (self.camera_movement[0] + turn,
-                                self.camera_movement[1] + pitch)
 
-    def move_camera(self, task):
-        self.camera_orbit.set_h(self.camera_orbit, self.camera_movement[0] * globalClock.get_dt() * 360.0 / 3.0)
-        new_pitch = self.camera_pitch.get_p() + self.camera_movement[1] * globalClock.get_dt() * 360.0 / 3.0
-        self.camera_pitch.set_p(min(max(new_pitch, -89), 89))
-        return Task.cont
+class RayThatCollidesWithScene:
+    def __init__(self):
+        self.setup_collision_ray() #  Now we have self.hitter_nodepath
+        self.queue = CollisionHandlerQueue()
+        self.traverser = CollisionTraverser('Collision Traverser')
+        self.traverser.showCollisions(base.render)
+        self.traverser.add_collider(self.hitter_nodepath, self.queue)
+        base.taskMgr.add(self.collide, "Collision Task")
+
+    def setup_collision_ray(self):
+        # Hitter. Do note that not every combination of object works,
+        # there is a table for that in the manual.
+        hitter = CollisionRay(0, 0, 0, 0, 1, 0)
+        self.hitter_nodepath = base.render.attach_new_node(CollisionNode('collision_hitter'))
+        self.hitter_nodepath.node().addSolid(hitter)
+        self.hitter_nodepath.set_pos(0, -10, 0)
+        self.hitter_nodepath.show()
+
+    def collide(self, task):
+        self.traverser.traverse(render)
+        for entry in self.queue.get_entries():
+            print(entry)
+        return task.cont
+
 
 if __name__ == '__main__':
-    app = Base()
+    app = Base(create_model=False)
+    from_object = RayThatCollidesWithScene()
+    into_object = SphereThatGetsCollidedWith()
     app.run()
 
