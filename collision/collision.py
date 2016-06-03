4fafdc97d1ee43f786f5352501656d0cd38b0f00
baribal <TheCheapestPixels@gmail.com>
2016-06-04 00:06:48 +0200

    Collision example now uses bitmasks.

--- collision/collision.py
+++ collision/collision.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 
-from math import sin
+from math import sin, cos, pi
 
-from panda3d.core import Vec3
+from panda3d.core import Vec3, BitMask32
 from panda3d.core import CollisionNode, CollisionSphere, CollisionRay
 from panda3d.core import CollisionHandlerQueue, CollisionTraverser
 
 from boilerplate.base import Base
+
+
+BM_LEFT = BitMask32(0x1)
+BM_RIGHT = BitMask32(0x10)
 
 
 class SphereThatGetsCollidedWith:
@@ -15,34 +19,52 @@ class SphereThatGetsCollidedWith:
         # not .show() it directly. Also, the sphere you see is a low-
         # poly model, but the collisions will be calculated against a
         # mathematically perfect sphere.
-        self.target = CollisionSphere(0, 0, 0, 1)
-        self.target_nodepath = base.render.attach_new_node(CollisionNode('collision_target'))
-        self.target_nodepath.node().addSolid(self.target)
-        self.target_nodepath.show()
+        self.targets = [self.create_sphere(model_name, bitmask)
+                        for model_name, bitmask in [("smiley", BM_LEFT), ("frowney", BM_RIGHT), ("jack", BM_LEFT | BM_RIGHT)]]
         base.taskMgr.add(self.move_collidee, "Moving Target Task")
+    
+    def create_sphere(self, model_name, bitmask):
+        model = loader.loadModel("models/" + model_name)
+        model.reparent_to(base.render)
+        target = CollisionSphere(0, 0, 0, 1)
+        target_node = CollisionNode('collision_target')
+        target_node.setIntoCollideMask(bitmask)
+        target_nodepath = model.attach_new_node(target_node)
+        target_nodepath.node().addSolid(target)
+        target_nodepath.show()
+        return model
 
     def move_collidee(self, task):
-        self.target_nodepath.set_pos(sin(task.time) * 3, 0, 0)
+        def circle_pos(t):
+            return Vec3(cos(t) * 3, 0, sin(t) * 3)
+        t_task = task.time
+        [target.set_pos(circle_pos(t_task + idx * pi*2./3.))
+         for idx, target in enumerate(self.targets)]
         return task.cont
 
 
 class RayThatCollidesWithScene:
     def __init__(self):
-        self.setup_collision_ray() #  Now we have self.hitter_nodepath
+        self.collision_rays = [self.setup_collision_ray(offset, bitmask)
+                               for offset, bitmask in [(-3, BM_LEFT), (3, BM_RIGHT)]]
         self.queue = CollisionHandlerQueue()
         self.traverser = CollisionTraverser('Collision Traverser')
         self.traverser.showCollisions(base.render)
-        self.traverser.add_collider(self.hitter_nodepath, self.queue)
+        for ray in self.collision_rays:
+            self.traverser.add_collider(ray, self.queue)
         base.taskMgr.add(self.collide, "Collision Task")
 
-    def setup_collision_ray(self):
+    def setup_collision_ray(self, offset, bitmask):
         # Hitter. Do note that not every combination of object works,
         # there is a table for that in the manual.
         hitter = CollisionRay(0, 0, 0, 0, 1, 0)
-        self.hitter_nodepath = base.render.attach_new_node(CollisionNode('collision_hitter'))
-        self.hitter_nodepath.node().addSolid(hitter)
-        self.hitter_nodepath.set_pos(0, -2, 0)
-        self.hitter_nodepath.show()
+        hitter_node = CollisionNode('collision_hitter')
+        hitter_node.setFromCollideMask(bitmask)
+        hitter_nodepath = base.render.attach_new_node(hitter_node)
+        hitter_nodepath.node().addSolid(hitter)
+        hitter_nodepath.set_pos(offset, -2, 0)
+        hitter_nodepath.show()
+        return hitter_nodepath
 
     def collide(self, task):
         self.traverser.traverse(render)
@@ -52,8 +74,7 @@ class RayThatCollidesWithScene:
 
 
 if __name__ == '__main__':
-    app = Base(create_model=True)
-    app.model.set_scale(0.2)
+    app = Base(create_model=False)
     from_object = RayThatCollidesWithScene()
     into_object = SphereThatGetsCollidedWith()
     app.run()
