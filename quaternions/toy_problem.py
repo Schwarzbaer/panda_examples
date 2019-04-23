246268a4ac2cdc5c3bf919454f5cfc2c78aeab0f
Sebastian Hoffmann <TheCheapestPixels@gmail.com>
2019-04-23 03:20:07 +0200

    Quat toy problem works in hpr space for low deltas

--- quaternions/toy_problem.py
+++ quaternions/toy_problem.py
@@ -44,29 +44,38 @@ mass.setAngularSleepThreshold(0)
 shape = BulletSphereShape(1)
 mass.add_shape(shape)
 mass_node = s.render.attach_new_node(mass)
-mass_node.set_hpr(90, 90, 0)
+mass_node.set_hpr(0, 0, 0)
 bullet_world.attach_rigid_body(mass)
 model = s.loader.load_model('models/smiley')
-model_node = model.reparent_to(mass_node)
-# model.setRenderModeWireframe()
+model.reparent_to(mass_node)
+model_axis = loader.load_model('models/zup-axis')
+model_axis.reparent_to(model)
+model_axis.set_pos(-1, -1, -1)
+model_axis.set_scale(0.2)
 
 
 target_node = s.loader.load_model('models/smiley')
 target_node.reparent_to(s.render)
-#target_node.set_scale(1.1)
-target_node.set_hpr(0, 20, -130)
-target_node.set_x(-2.5)
-#target_node.set_render_mode_wireframe()
+target_node.set_h(45)
+target_node.set_scale(1.5)
+target_node.set_render_mode_wireframe()
 
 
-speed_node = s.loader.load_model('models/frowney')
+delta_node = s.render.attach_new_node('delta')
+delta_node.set_pos(2, -1, -1)
+delta_node_model = s.loader.load_model('models/smiley')
+delta_node_model.reparent_to(delta_node)
+delta_node_model.set_h(180)
+delta_node_model.set_render_mode_wireframe()
+delta_node_model.set_two_sided(True)
+
+
+speed_node = s.loader.load_model('models/zup-axis')
 speed_node.reparent_to(s.render)
-#speed_node.set_scale(1.1)
-speed_node.set_x(2.5)
-#speed_node.set_render_mode_wireframe()
+speed_node.set_pos(2, -1, 1)
+speed_node.set_hpr(10, 0, 0)
 
 
-stabilizer_on = False
 def stabilize(task):
     # inertia: kg * m^2
     # torque: N*m
@@ -78,61 +87,55 @@ def stabilize(task):
 
     dt = globalClock.dt
 
-    tau = 0.1
+    tau = 0.2
     inertia = 1.0
 
     orientation = mass_node.get_quat(s.render)
-    angular_velocity = mass.get_angular_velocity() # In radians
+    angular_velocity = mass.get_angular_velocity() # radians / sec
+    # speed_node.set_scale(angular_velocity / (2*pi) * 0.05 + VBase3(0.01, 0.01, 0.01))
 
-    speed_node.set_hpr(angular_velocity / (2*pi) * 360)
+    delta_hpr = target_node.get_hpr(mass_node)
+    delta_hpr.componentwise_mult(VBase3(1, -1, 1))
+    delta_node.set_hpr(delta_hpr)
+    delta_angle = delta_node.get_quat().get_angle_rad()
 
-    delta_orientation_hpr = target_node.get_hpr(mass_node)
-    axis_of_torque_obj = VBase3(
-        delta_orientation_hpr.y,
-        delta_orientation_hpr.z,
-        delta_orientation_hpr.x,
-    ) / 360
-    axis_of_torque = s.render.get_relative_vector(
-        mass_node,
-        axis_of_torque_obj,
-    )
-    delta_angle = target_node.get_quat(mass_node).get_angle_rad()
-
-    # if delta_angle > 0.01:
-    #     axis_of_torque = VBase3(
-    #         delta_orientation.x,
-    #         delta_orientation.y,
-    #         delta_orientation.z,
-    #     )
-    #     axis_of_torque /= axis_of_torque.length() # normalized
-    # else:
-    #     axis_of_torque = VBase3(0, 0, 0)
-
-    target_angular_velocity = axis_of_torque * (delta_angle / tau)
+    axis_of_torque = VBase3(
+        -delta_node.get_p(s.render),
+        delta_node.get_r(s.render),
+        delta_node.get_h(s.render),
+    ) / 180
+    # speed_node.set_scale(axis_of_torque / (2*pi) * 0.1)
+    target_angular_velocity = axis_of_torque * delta_angle / tau
     steering_impulse = target_angular_velocity * inertia
 
-    # First we calculate the impulse that cancels out all current rotation,
+    # We calculate the impulse that cancels out all current rotation,
     # which is noise with regard to the intended rotation.
     countering_impulse = -angular_velocity / 2.5
 
     # We sum up these impulses. Thus, one frame's steering impulse will be
     # canceled out in the next frame as being noise movement.
-    impulse = steering_impulse + countering_impulse
 
-    # TODO: Clamp impulse magnitude
-    #if impulse.length() > 0.4:
-    #    impulse = impulse / impulse.length() * 0.4
+    impulse = countering_impulse * 0.2 + steering_impulse * 0.8
+    # impulse = countering_impulse
+    # impulse = steering_impulse * 0.5
+    # impulse = VBase3(0, 0, 0)
 
-    mass.apply_torque_impulse(impulse * dt)
+    max_impulse = 0.1
+    if impulse.length() > max_impulse:
+        impulse = impulse / impulse.length() * max_impulse
+
+    mass.apply_torque_impulse(impulse)
     return task.cont
 s.task_mgr.add(stabilize, sort=0)
 
 
 # Apply torque with 't'
-def add_torque():
+def add_torque(x=0, y=0, z=0):
     # This happens in world space
-    mass.apply_torque_impulse(VBase3(1, 0, 0))
-s.accept('t', add_torque)
+    mass.apply_torque_impulse(VBase3(x, y, z))
+s.accept('x', add_torque, [1, 0, 0])
+s.accept('y', add_torque, [0, 1, 0])
+s.accept('z', add_torque, [0, 0, 1])
 
 
 s.run()
