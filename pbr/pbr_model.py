353ba3b1c0fce9d3ae9a054574740e4482780fde
Sebastian Hoffmann <TheCheapestPixels@gmail.com>
2023-10-25 20:32:56 +0200

    Went over pbr_model.py

--- pbr/pbr_model.py
+++ pbr/pbr_model.py
@@ -1,8 +1,11 @@
-# Hi! Wanna learn about the nitty-gritty of PBR models and textures?
-# Let's go!
+# Hi! Wanna learn about the nitty-gritty of PBR models and textures, or
+# at least with textures in principle? Have you read
+# `geometry/basic_model.py`, so that you know how a model is generated
+# in general?
+# Nice, let's go!
 
 from math import sin, cos, pi, sqrt, ceil
-from colorsys import hsv_to_rgb
+from colorsys import hls_to_rgb
 import random
 
 from panda3d.core import NodePath
@@ -24,16 +27,12 @@ from panda3d.core import SamplerState
 ### MODEL
 ###
 
-# We'll keep things quite simple with the model, as the interesting bits
-# happen in the textures anyway. Here we will create a simple square on
-# the x/z plane, just like CardMaker would create.
-#
 # A PBR-using model is no different from any other kind of model, it
 # just uses a specific vertex format, textures, and materials so as to
 # satisfy the needs of PBR shaders.
 #
-# First we have to specify the format, because Panda3D doesn't come with
-# a stock one for our needs.
+# First we have to specify the format, this time because Panda3D doesn't
+# come with a stock one for our needs.
 # There will be five columns:
 # * `vertex`: The position of the vertex in model space.
 # * `texcoord`: The UV coordinate of the texture at this vertex.
@@ -57,53 +56,51 @@ v_format = GeomVertexFormat()
 v_format.add_array(v_array_format)
 v_format = GeomVertexFormat.register_format(v_format)
 
-# Now we can create a heap to throw our data onto.
+# And again, the data table and the writers.
 v_data = GeomVertexData("Data", v_format, Geom.UHStatic)
-v_data.reserve_num_rows(4)
+v_data.unclean_set_num_rows(4)
 vertex = GeomVertexWriter(v_data, InternalName.get_vertex())
 normal = GeomVertexWriter(v_data, InternalName.get_normal())
 texcoord = GeomVertexWriter(v_data, InternalName.get_texcoord())
 tangent = GeomVertexWriter(v_data, InternalName.get_tangent())
 binormal = GeomVertexWriter(v_data, InternalName.get_binormal())
 
-# And now comes the data itself, all four vertices of it...
+# And the same four vertices, just with another data set.
 # Top Left
-vertex.add_data3f(-1, 0, 1)
-normal.add_data3f(0, -1, 0)
-texcoord.add_data2f(0, 1)
-tangent.add_data4f(1, 0, 0, 1)
-binormal.add_data4f(0, 0, 1, 1)
+vertex.set_data3f(-1, 0, 1)
+texcoord.set_data2f(0, 1)
+normal.set_data3f(0, -1, 0)
+binormal.set_data4f(0, 0, 1, 1)
+tangent.set_data4f(1, 0, 0, 1)
 
 # Top Right
-vertex.add_data3f(1, 0, 1)
-normal.add_data3f(0, -1, 0)
-texcoord.add_data2f(1, 1)
-tangent.add_data4f(1, 0, 0, 1)
-binormal.add_data4f(0, 0, 1, 1)
+vertex.set_data3f(1, 0, 1)
+texcoord.set_data2f(1, 1)
+normal.set_data3f(0, -1, 0)
+binormal.set_data4f(0, 0, 1, 1)
+tangent.set_data4f(1, 0, 0, 1)
 
 # Bottom Left
-vertex.add_data3f(-1, 0, -1)
-normal.add_data3f(0, -1, 0)
-texcoord.add_data2f(0, 0)
-tangent.add_data4f(1, 0, 0, 1)
-binormal.add_data4f(0, 0, 1, 1)
+vertex.set_data3f(-1, 0, -1)
+texcoord.set_data2f(0, 0)
+normal.set_data3f(0, -1, 0)
+binormal.set_data4f(0, 0, 1, 1)
+tangent.set_data4f(1, 0, 0, 1)
 
 # Bottom Right
-vertex.add_data3f(1, 0, -1)
-normal.add_data3f(0, -1, 0)
-texcoord.add_data2f(1, 0)
-tangent.add_data4f(1, 0, 0, 1)
-binormal.add_data4f(0, 0, 1, 1)
+vertex.set_data3f(1, 0, -1)
+texcoord.set_data2f(1, 0)
+normal.set_data3f(0, -1, 0)
+binormal.set_data4f(0, 0, 1, 1)
+tangent.set_data4f(1, 0, 0, 1)
 
-# And now we add the two triangles.
+# And the same two triangles again.
 tris = GeomTriangles(Geom.UHStatic)
 tris.add_vertices(2, 1, 0)
 tris.add_vertices(2, 3, 1)
 tris.close_primitive()
 
-# Now we store the vertex data and primitives in a Geom, store that in
-# a GeomNode, and store that in turn in a NodePath, so it can be readily
-# attached to the scene graph.
+# ...and putting it all together... again.
 geom = Geom(v_data)
 geom.add_primitive(tris)
 node = GeomNode('geom_node')
@@ -114,11 +111,11 @@ surface = NodePath(node)
 ### MATERIAL
 ###
 
-# Materials are metavariables affecting how a pack of textures is
-# rendered. Specifically, we can multiply the color texture
+# Materials are values affecting how a pack of textures is rendered.
+# Specifically, we can multiply the color texture
 # (`TextureStage.Modulate`) with an arbitrary RGBA value; Typically
 # we'll just multiply it with 1.0 though. The same goes for the emission
-# texture.
+# texture. For details on what those are, see below.
 surface_material = Material()
 surface_material.set_diffuse((1,1,1,1))
 surface_material.set_emission((1,1,1,1))
@@ -128,13 +125,13 @@ surface.set_material(surface_material)
 ### TEXTURES
 ###
 
-# First we decide on the size of the textures.
-resolution = 256
-
-# Now we start setting up our textures by filling images with default
-# values, and then manipulate them a bit.
-# * "Base color" is an easy one; Just put in the color you want. In this
-#   case we'll use a very light gray.
+# Textures are images that get mapped onto meshes; Each vertex has a u/v
+# coordinate, where u is the to-the-right axis and v the upwards one.
+# They also have properties like the TextureStage, which specifies how
+# the texture is to be used. The trivial case is to use a simple RGB
+# color image and be done with it. In our case, we have four types of
+# texture:
+# * "Base color" is an easy one, that is just the basic color image.
 # * "Occlusion roughness metallicity" is obviously three values in one
 #   map.
 #   * Occlusion refers to ambient occlusion, which darkens areas for
@@ -158,7 +155,11 @@ resolution = 256
 #   appeal; Also to have done it, because the trivial case is boring.
 # * "Emissions" covers glowing parts. This is an RGB map.
 
-# First, a texture stage type to RGB tuple mapping.
+# Before we do anything else, let's decide on the size of the textures.
+resolution = 256
+
+# First then, we create a list of default values for each image that we
+# create.
 stages = [
     # Base color; Near black.
     (TextureStage.MModulate, (0.01, 0.01, 0.01)),
@@ -182,9 +183,11 @@ for mode, values in stages:
     # ... create the texture, and apply it to the surface.
     texture = Texture('pbr_color_texture')
     surface.set_texture(stage, texture)
-    # FIXME: Remove this.
+    # FIXME: Why do these have no effect?
     #texture.set_magfilter(SamplerState.FT_nearest)
     #texture.set_minfilter(SamplerState.FT_nearest)
+    #texture.set_wrap_u(Texture.WM_clamp)
+    #texture.set_wrap_v(Texture.WM_clamp)
     #texture.set_anisotropic_degree(4)
     # We do NOT load the images into the textures yet, as we first want
     # to play around with them a bit more. We're here for the cool
@@ -198,11 +201,16 @@ for mode, values in stages:
 # the file will be about fun with pixel values, so if you're just
 # interested in the basics of PBR, you are now done; Congratulations.
 
-# Now let us tile our black square with spectral-colored glittery tiles.
-tile_size = 8  # Tile height / width in texels
-ratio = 0.5  # Ratio of the diamond's size to the tile's size
+# Now let us consider our black square as a set of tiles. We will color
+# every second tile in spectral-colored glitter.
+
+tile_size = 16  # Size of tile in texels
 max_glitter_angle = 8.0 / 360.0 * 2.0 * pi  # 8.0 degree in radians
 
+# A source for normal map values; They are random within the angle
+# specified above.
+# FIXME: This needs to change to something where we can see the normals
+# explicitly pointing towards the intended directions.
 def glitter_normal():
     # First, we need a random value between -1 and 1, and scale it by
     # the maximum angle for the glitter.
@@ -216,12 +224,14 @@ def glitter_normal():
     normal_value = sqrt(1.0 - tangent_value ** 2 - binormal_value ** 2)
     return (tangent_value / 2.0 + 0.5, binormal_value / 2.0 + 0.5, normal_value / 2.0 + 0.5)
 
-# Let's quickly precompute some values...
-radius = float(tile_size) / 2.0 * ratio
-center = float(tile_size) / 2.0 + 0.5 
+# And a similar, simpler helper for the color.
+# FIXME: The latest in perceptually smooth color models is OKLab.
+def glitter_color():
+    return hls_to_rgb(random.random(), 0.5, 1.0)
+
+# Let's quickly precompute some values and create the palette to use.
 num_tiles = ceil(resolution / tile_size)
-tile_data = {(x, y): (hsv_to_rgb(random.choice([x/6. for x in range(6)]), 1.0, 1.0),
-                      glitter_normal())
+tile_data = {(x, y): (glitter_color(), glitter_normal())
              for x in range(0, num_tiles)
              for y in range(0, num_tiles)
              }
@@ -236,12 +246,12 @@ emit = image_to_texture_mapping[3][0]
 for x in range(0, resolution):
     for y in range(0, resolution):
         tile_id = (x // tile_size, y // tile_size)
-        tile_x = x % tile_size
-        tile_y = y % tile_size
-        # If this texel is close enough to the center, ...
-        if abs(tile_x-center) < radius and abs(tile_y-center) < radius:
+        tile_x, tile_y = tile_id
+        # If this texel is tile is to be colored, ...
+        if (tile_x + tile_y) % 2 == 0:
             # ...we modify it to the precomputed values.
             texel_color, texel_normal = tile_data[tile_id]
+            #texel_normal = (0.5, 0.5, 1.0)
             color.set_xel(x, y, *texel_color)
             normal.set_xel(x, y, *texel_normal)
             # ...and make it a bit less polished, and non-metallic.
