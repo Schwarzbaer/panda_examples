9e6dd31aa8b4152ecf41d98ebb895dec2d24a686
Sebastian Hoffmann <TheCheapestPixels@gmail.com>
2023-10-27 03:49:14 +0200

    Nearly happy with bones example...

--- bones/bones_model.py
+++ bones/bones_model.py
@@ -1,3 +1,7 @@
+# Have you read the geometry example yet to learn how you generate a
+# static model? Do you want to give it a skeleton and make it move? You
+# have come to the right tutorial.
+
 from panda3d.core import Vec3
 from panda3d.core import Mat4
 from panda3d.core import NodePath
@@ -17,6 +21,7 @@ from panda3d.core import TransformBlendTable
 from panda3d.core import TransformBlend
 from panda3d.core import JointVertexTransform
 from panda3d.core import SparseArray
+
 from direct.actor.Actor import Actor
 
 #
@@ -32,13 +37,14 @@ vertices_per_circle = 30
 circle_radius = 0.25
 segments = 20
 length = 4.0
-segment_height = length / segments
 
 # ...and data derived from it
+segment_height = length / segments
 num_rows = (vertices_per_circle + 1) * (segments + 1)
 num_tris = segments * (vertices_per_circle - 1) * 2
 
-# Setting up the columns for our vertex data.
+# Setting up the columns for our vertex data. This time we have a column
+# to essentially point to entries in a table to; More on that later.
 v_array_format = GeomVertexArrayFormat()
 v_array_format.add_column(InternalName.get_vertex(),          3, Geom.NT_float32, Geom.C_point)
 v_array_format.add_column(InternalName.get_color(),           4, Geom.NT_float32, Geom.C_color)
@@ -64,9 +70,32 @@ transform = GeomVertexWriter(v_data, InternalName.get_transform_blend())
 tris = GeomTriangles(Geom.UHStatic)
 tris.reserve_num_vertices(num_tris * 3)
 
+# Okay, new containers!
+# A `Character` contains `CharacterJoint`s; These are the basic unit of
+# animation. Like nodes in a scene graph, they form a tree structure and
+# have a transformation that moves / rotates them relative to their
+# parent. These joints can be exposed to attach NodePaths to them, or
+# controlled to move them around like NodePaths.
+# There are further details to the `Character` which we will skip for
+# now.
+# At the other end of the animation system are the vertices that are
+# moved around when joints move.  The vertex table contains a column
+# that tells what row of the `TransformBlendTable` to use for this
+# vertex row. Each row of that table contains a `TransformBlend`, which
+# is a weighted list of `JointVertexTransforms`, which each reference a
+# `CharacterJoint`. So each time that a model is animated, its joints
+# get moved around, these movements are weighted and averaged, and then
+# vertices get moved around accordingly.
+# Now, the details of `Character` that we skipped.
+# FIXME: Character, bundle, CharacterJoint (PartGroup, name, matrix)
 character = Character("model")
 bundle = character.get_bundle(0)
 blend_table = TransformBlendTable()
+# The vertex rows to be animated have to be explicitly "switched on".
+blend_table.set_rows(SparseArray.lower_on(num_rows))
+# We need to tell our vertex data about that table that its
+# `transform_blend` column refers to.
+v_data.set_transform_blend_table(blend_table)
 
 # With just two NodePaths, a lot of math can be avoided.
 reference_space = NodePath('ref_space')
@@ -74,15 +103,24 @@ turtle = reference_space.attach_new_node('turtle')
 
 # Now we actually dump the data into their containers.
 for circle in range(segments + 1):
-    # Joints a.k.a. bones first.
+    # Joints first. They sit in the center of the circles.
     if circle == 0:  # Model root
         joint = CharacterJoint(character, bundle, PartGroup(bundle, "<skeleton>"), f"joint_{circle}", Mat4.ident_mat())
     else:
+        # The transformations are relative to the parent, so we will use
+        # the `segment_height` here, but multiples of it for calculating
+        # the vertex data.
         turtle.set_pos(0, 0, segment_height)
         turtle.set_hpr(0, 0, 0)
         mat = turtle.get_transform(reference_space).get_mat()
         joint = CharacterJoint(character, bundle, parent_joint, f"joint_{circle}", mat)
-    parent_joint = joint
+    parent_joint = joint  # For the next time around.
+    # Each joint controls a circle of vertices, and those vertices are
+    # controlled only by the ring, so we need one `TransformBlend` per
+    # circle.
+    blend = TransformBlend()
+    blend.add_transform(JointVertexTransform(joint), weight=1)
+    bt_index = blend_table.add_blend(blend)
 
     # Vertex and triangle data
     for vertex_in_circle in range(vertices_per_circle + 1):
@@ -90,16 +128,12 @@ for circle in range(segments + 1):
         # Nudge the turtle into position, ...
         turtle.set_pos(0, 0, circle * segment_height)
         turtle.set_hpr(vertex_in_circle * 360.0 / vertices_per_circle, 0, 0)
-        # ...get and write the data for position and color, ...
+        # ...get and write the data for position, color and the
+        # applicable row of the blend table, ...
         local_radius = circle_radius * (1 - circle / segments)
         v_pos = reference_space.get_relative_point(turtle, Vec3(0, local_radius, 0))
         vertex.set_data3f(v_pos)
-        #color.set_data4f(circle/segments, 0, vertex_in_circle/vertices_per_circle, 1)
         color.set_data4f(vertex_in_circle % 2, circle % 2, 0, 1)
-        # ...and create the transform blend table entry and reference it, ...
-        blend = TransformBlend()
-        blend.add_transform(JointVertexTransform(joint), weight=1)
-        bt_index = blend_table.add_blend(blend)
         transform.set_data1i(bt_index)
         # ...and fill in the triangles (where applicable).
         if circle > 0 and vertex_in_circle > 0:
@@ -111,54 +145,14 @@ for circle in range(segments + 1):
             tris.add_vertices(tl, br, tr)
             tris.add_vertices(bl, br, tl)
 
-
-# FIXME: WTF?
-blend_table.set_rows(SparseArray.lower_on(v_data.get_num_rows()))
-v_data.set_transform_blend_table(blend_table)
-
-            
 # Putting the GeomNode together...
 geom = Geom(v_data)
 geom.add_primitive(tris)
 node = GeomNode('geom_node')
 node.add_geom(geom)
 
-# ...and wrapping it up for the scene graph
+# ...and wrapping it up for the scene graph.
 node_np = NodePath(node)
 character_np = NodePath(character)
 node_np.reparent_to(character_np)
 actor = Actor(character_np)
-
-
-#
-# Application
-#
-
-max_angle = 90.0*2
-side_phases = 1.5
-front_phases = 0.7
-
-import sys
-from math import pi
-from math import sin
-from direct.showbase.ShowBase import ShowBase
-
-ShowBase()
-base.accept('escape', sys.exit)
-base.cam.set_pos(0, -10, 3)
-base.cam.look_at(0, 0, 2.5)
-
-actor.reparent_to(base.render)
-joints = [actor.control_joint(None, "modelRoot", f"joint_{i}") for i in range(1, segments)]
-
-def undulate_tentacle(task):
-    for j_idx, j_node in enumerate(joints):
-        side_phase_offset = side_phases / (segments - 1) * j_idx * 2 * pi
-        front_phase_offset = front_phases / (segments - 1) * j_idx * 2 * pi
-        #j_node.set_r(sin(task.time) * max_angle / (segments - 1))
-        j_node.set_r(sin(task.time + side_phase_offset) * max_angle / (segments - 1))
-        #j_node.set_p(sin(task.time + front_phase_offset) * max_angle / (segments - 1))
-    return task.cont
-
-base.task_mgr.add(undulate_tentacle)
-base.run()
---
+++ bones/main.py
@@ -0,0 +1,37 @@
+import sys
+from math import pi
+from math import sin
+from direct.showbase.ShowBase import ShowBase
+
+from bones_model import actor
+from bones_model import segments
+
+
+ShowBase()
+base.accept('escape', sys.exit)
+base.cam.set_pos(0, -10, 3)
+base.cam.look_at(0, 0, 2.5)
+
+# This is all that we need to do to get our actor into the scene.
+actor.reparent_to(base.render)
+
+# Some parameters to control the undulation of the tentacle.
+max_angle = 120.0
+side_speed = 1.0
+side_phases = 0.7
+front_speed = 1.1
+front_phases = 0.0
+
+# We animate the model by controlling its joints directly.
+joints = [actor.control_joint(None, "modelRoot", f"joint_{i}") for i in range(1, segments)]
+
+def undulate_tentacle(task):
+    for j_idx, j_node in enumerate(joints):
+        side_phase_offset = side_phases / (segments - 1) * j_idx * 2 * pi
+        front_phase_offset = front_phases / (segments - 1) * j_idx * 2 * pi
+        j_node.set_r(sin(task.time * side_speed + side_phase_offset) * max_angle / (segments - 1))
+        j_node.set_p(sin(task.time * front_speed + front_phase_offset) * max_angle / (segments - 1))
+    return task.cont
+
+base.task_mgr.add(undulate_tentacle)
+base.run()
